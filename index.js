e0a582f742a2972afbc5756820494a1540e6e00f
IvanRybachuk <68922258+IvanRybachuk@users.noreply.github.com>
2020-10-29 22:06:08 +0300

    1
    1

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-startBtn = document.querySelector("#btnStart");
-stopBtn = document.querySelector("#btnStop");
-(colors = ["#FFFFFF", "#2196F3", "#4CAF50", "#FF9800", "#009688", "#795548"]),
-  (randomIntegerFromInterval = (max) => {
+const startBtn = document.querySelector("#btnStart");
+const stopBtn = document.querySelector("#btnStop");
+const colors = ["#FFFFFF", "#2196F3", "#4CAF50", "#FF9800", "#009688", "#795548"],
+  randomIntegerFromInterval = (max) => {
     return Math.floor(Math.random() * (max + 1));
-  }),
-  (setRandomColor = () => {
+  },
+  setRandomColor = () => {
     const color = colors[randomIntegerFromInterval(colors.length - 1)];
     console.log(color);
     document.body.style.backgroundColor = color;
-  });
+  };
 
 let interval = undefined;
 
